4d02c016cee930bb9d6d4504d9c5503946eb48e1
my-prog <61547117+my-prog@users.noreply.github.com>
2020-03-03 17:07:54 +0300

    Two balls program with Struct

--- Ball6_controlled _collision.cpp
+++ Ball6_controlled _collision.cpp
@@ -1,22 +1,36 @@
 //controlled collision
 //Управляемое столкновение двух шаров
-//для Юли ;-))
+
 
  #include "TXLib.h"
 
  #define X 800
  #define Y 600
 
- void MoveBall (double x1, double y1, double Vx1, double Vy1,
-                double x2, double y2, double Vx2, double Vy2);
+ struct Ball
+    {
+    double  x,  y;
+    double Vx, Vy;
+    double ax, ay;
+    double r;
+    };
 
- void Phisics  (double* x, double* y, double* Vx, double* Vy, double ax = 0, double ay = 0, bool Ftrenia = false, double dt = 2, double r = 20);
- void DrawBall (double x, double y, double r, COLORREF colorLine, double width, COLORREF colorFon);
+struct Drawww
+    {
+    COLORREF colorLine;
+    double width;
+    COLORREF colorFon;
+    };
 
- void BarrierArrow (double* x, double* y);
- void BarrierWASZ  (double* x, double* y);
+ void MoveBall (Ball ball1, Ball ball2);
 
- double Distance (double x1, double y1, double x2, double y2);
+ void Phisics  (Ball* ball, bool Ftrenia = false, double dt = 2);
+ void DrawBall (Ball ball, Drawww coloring);
+
+ void BarrierArrow (Ball* ball);
+ void BarrierWASZ  (Ball* ball);
+
+ double Distance (Ball ball1, Ball ball2);
  void DrawText (double x, double y, const char* text, int size, COLORREF color);
 
  int main()
@@ -26,57 +40,68 @@
     txSetFillColor (TX_BLACK);
     txClear();
 
-    MoveBall (10, 10, 2, 5, 700, 500, -3, 2);
+    Ball ball1 = { 10,  10,  2, 5, 0, 0, 20};
+    Ball ball2 = {700, 500, -3, 2, 0, 0, 20};
+    Ball ball3 = {200, 200,  2, 2, 0, 0, 10};
+    Ball ball4 = {700, 500, -3, 2, 0, 0, 20};
+
+    MoveBall (ball1, ball2);
 
     return 0;
     }
 
-void MoveBall (double x1, double y1, double Vx1, double Vy1,
-               double x2, double y2, double Vx2, double Vy2)
+void MoveBall (Ball ball1, Ball ball2)
     {
-    double r1 = 20, width1 = 2, r2 = 20, width2 = 2,
-           oldx1 = x1, oldy1 = y1, oldx2 = x2, oldy2 = y2;
+    double width1 = 2, width2 = 2,
+           oldx1 = ball1.x, oldy1 = ball1.y, oldx2 = ball2.x, oldy2 = ball2.y;
 
     COLORREF colorLine1 = RGB (255, 255,   0),
              colorFon1  = RGB ( 11, 206,   0),
              colorLine2 = RGB (155, 255, 155),
              colorFon2  = RGB (  0,  37, 205);
 
-    while ((Distance (x1, y1, x2, y2) >= r1 + r2) && (!(txGetAsyncKeyState (VK_ESCAPE))))
+    Drawww coloring1   = {RGB (255, 255,   0), 2, RGB ( 11, 206,   0)},
+           coloring2   = {RGB (155, 255, 155), 2, RGB (  0,  37, 205)},
+           coloringWin = {TX_RED, 5, TX_RED};
+
+    while ((Distance (ball1, ball2) >= ball1.r + ball2.r) && (!(txGetAsyncKeyState (VK_ESCAPE))))
         {
         if (txGetAsyncKeyState (VK_SPACE))
             {
-            Vx1 = 0;
-            Vy1 = 0;
+            ball1.Vx = 0;
+            ball1.Vy = 0;
+            ball2.Vx = 0;
+            ball2.Vy = 0;
             }
 
-        oldx1 = x1, oldy1 = y1, oldx2 = x2, oldy2 = y2;
+        Ball old1 = ball1, old2 = ball2;
 
-        BarrierArrow (&x1, &y1);
-        Phisics      (&x1, &y1, &Vx1, &Vy1);
+        BarrierArrow (&ball1);
+        Phisics      (&ball1);
 
-        DrawBall (x1, y1, r1, colorLine1, width1, colorFon1);
+        DrawBall (ball1, coloring1);
 
-        BarrierWASZ  (&x2, &y2);
-        Phisics      (&x2, &y2, &Vx2, &Vy2);
+        BarrierWASZ  (&ball2);
+        Phisics      (&ball2);
 
-        DrawBall (x2, y2, r2, colorLine2, width2, colorFon2);
+        DrawBall (ball2, coloring2);
 
         txSleep (20);
-        COLORREF colorWin;
 
-        if (Distance (x1, y1, x2, y2) <= r1 + r2)
+        if (Distance (ball1, ball2) <= ball1.r + ball2.r)
             {
             char win = '?';
-            if (Distance (x1, y1, oldx1, oldy1) < Distance (x2, y2, oldx2, oldy2))
+            if (Distance (ball1, old1) < Distance (ball2, old2))
                 {
                 win = '1';
-                colorWin = colorFon1;
+                coloringWin.colorFon  = coloring1.colorFon;
+                coloringWin.colorLine = coloring1.colorLine;
                 }
-            else if (Distance (x1, y1, oldx1, oldy1) > Distance (x2, y2, oldx2, oldy2))
+            else if (Distance (ball1, old1) > Distance (ball2, old2))
                     {
                     win = '2';
-                    colorWin = colorFon2;
+                    coloringWin.colorFon  = coloring2.colorFon;
+                    coloringWin.colorLine = coloring2.colorLine;
                     }
                  else
                     {
@@ -89,70 +114,70 @@ void MoveBall (double x1, double y1, double Vx1, double Vy1,
             sprintf (s, "... Game Over. Winner %c player", win);
             DrawText (50, 50, s, 42, TX_WHITE);
 
-
-            DrawBall (x2, y2, r2*5, colorLine2, width2, colorWin);
-            DrawText (x2, y2, "BOOM", 48, TX_GREEN);
+            ball2.r *= 5;
+            DrawBall (ball2, coloringWin);
+            DrawText (ball2.x - ball2.r/2, ball2.y, "BOOM", 48, TX_GREEN);
             }
         }
     }
 
- void Phisics (double* x, double* y, double* Vx, double* Vy, double ax, double ay, bool Ftrenia, double dt, double r)
+ void Phisics (Ball* ball, bool Ftrenia, double dt)
     {
-    if (Ftrenia == true) { *Vx *= 0.999; }
+    if (Ftrenia == true) { (*ball).Vx *= 0.999; }
 
-    *x += (*Vx)*dt + ax*dt*dt/2;
-    *y += (*Vy)*dt + ay*dt*dt/2;
+    (*ball).x += ((*ball).Vx)*dt + ((*ball).ax)*dt*dt/2;
+    (*ball).y += ((*ball).Vy)*dt + ((*ball).ay)*dt*dt/2;
 
-    if (*x + r > X)
+    if ((*ball).x + (*ball).r > X)
         {
-        *x  = X - r;
-        *Vx = -(*Vx);
+        (*ball).x  = X - (*ball).r;
+        (*ball).Vx = -((*ball).Vx);
         }
-    if (*y + r > Y)
+    if ((*ball).y + (*ball).r > Y)
         {
-        *y  = Y - r;
-        *Vy = -(*Vy);
+        (*ball).y  = Y - (*ball).r;
+        (*ball).Vy = -((*ball).Vy);
         }
 
-    if (*x - r < 0)
+    if ((*ball).x - (*ball).r < 0)
         {
-        *x  = r;
-        *Vx = -(*Vx);
+        (*ball).x  = (*ball).r;
+        (*ball).Vx = -((*ball).Vx);
         }
-    if (*y - r < 0)
+    if ((*ball).y - (*ball).r < 0)
         {
-        *y  = r;
-        *Vy = - (*Vy);
+        (*ball).y  = (*ball).r;
+        (*ball).Vy = - ((*ball).Vy);
         }
     }
 
- void BarrierArrow (double* x, double* y)
+ void BarrierArrow (Ball* ball)
     {
-    if (txGetAsyncKeyState (VK_LEFT))   { *x -= 10; }
-    if (txGetAsyncKeyState (VK_RIGHT))  { *x += 10; }
-    if (txGetAsyncKeyState (VK_UP))     { *y -= 10; }
-    if (txGetAsyncKeyState (VK_DOWN))   { *y += 10; }
+    if (txGetAsyncKeyState (VK_LEFT))   { (*ball).x -= 10; }
+    if (txGetAsyncKeyState (VK_RIGHT))  { (*ball).x += 10; }
+    if (txGetAsyncKeyState (VK_UP))     { (*ball).y -= 10; }
+    if (txGetAsyncKeyState (VK_DOWN))   { (*ball).y += 10; }
     }
 
- void BarrierWASZ (double* x, double* y)
+ void BarrierWASZ (Ball* ball)
     {
-    if (txGetAsyncKeyState ('A') || txGetAsyncKeyState ('a'))  { *x -= 10; }
-    if (txGetAsyncKeyState ('D') || txGetAsyncKeyState ('d'))  { *x += 10; }
-    if (txGetAsyncKeyState ('S') || txGetAsyncKeyState ('s'))  { *y += 10; }
-    if (txGetAsyncKeyState ('W') || txGetAsyncKeyState ('w'))  { *y -= 10; }
+    if (txGetAsyncKeyState ('A') || txGetAsyncKeyState ('a'))  { (*ball).x -= 10; }
+    if (txGetAsyncKeyState ('D') || txGetAsyncKeyState ('d'))  { (*ball).x += 10; }
+    if (txGetAsyncKeyState ('S') || txGetAsyncKeyState ('s'))  { (*ball).y += 10; }
+    if (txGetAsyncKeyState ('W') || txGetAsyncKeyState ('w'))  { (*ball).y -= 10; }
     }
 
- void DrawBall (double x, double y, double r, COLORREF colorLine, double width, COLORREF colorFon)
+ void DrawBall (Ball ball, Drawww coloring)
     {
-    txSetColor     (colorLine, width);
-    txSetFillColor (colorFon);
+    txSetColor     (coloring.colorLine, coloring.width);
+    txSetFillColor (coloring.colorFon);
 
-    txCircle  (x, y, r);
+    txCircle  (ball.x, ball.y, ball.r);
     }
 
- double Distance (double x1, double y1, double x2, double y2)
+ double Distance (Ball ball1, Ball ball2)
     {
-    return sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+    return sqrt((ball1.x - ball2.x)*(ball1.x - ball2.x) + (ball1.y - ball2.y)*(ball1.y - ball2.y));
     }
 
 void DrawText(double x, double y, const char* text, int size, COLORREF color)
